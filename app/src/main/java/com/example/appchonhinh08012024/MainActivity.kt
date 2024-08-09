40812a9ffbcaf00f7d706235f2531b89b35bf586
phatdroid94 <phatdroid94@gmail.com>
2024-08-09 18:54:28 +0700

    randomimage

--- app/src/main/java/com/example/appchonhinh08012024/MainActivity.kt
+++ app/src/main/java/com/example/appchonhinh08012024/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.appchonhinh08012024
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private var imgRandom: ImageView? = null
+    private var imgUserSelect: ImageView? = null
+    private val arrNameAnimals by lazy {
+        resources.getStringArray(R.array.array_animal)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imgRandom = findViewById(R.id.image_view_random)
+        imgUserSelect = findViewById(R.id.image_view_user_select)
+
+        val indexRandom = Random.nextInt(arrNameAnimals.size)
+        val resourceRandom = resources.getIdentifier(arrNameAnimals[indexRandom], "drawable", packageName)
+        imgRandom?.setImageResource(resourceRandom)
     }
 }
